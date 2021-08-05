768d95ab34b13d12add36027a06c8abdd3ad1175
luoxikui <luoxikui@emay.cn>
2021-08-05 17:33:35 +0800

    用户列表

--- routers/routes/public/userList.js
+++ routers/routes/public/userList.js
@@ -1,8 +1,8 @@
-const { getUserList } = require("../../../db/mysql/public/user");
+const { sqlGetUserList } = require("../../../db/mysql/public/user");
 
 module.exports = async (ctx, next) => {
   const {pageNum = 1, pageSize = 10} = ctx.query
-  let res = await getUserList({
+  let res = await sqlGetUserList({
     pageNum,
     pageSize
   });
