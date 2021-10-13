7d92631358d0a08dc0396c31b4ae2b0342e3ee98
luoxikui <luoxikui@emay.cn>
2021-10-13 17:40:56 +0800

    路径不存在返回404

--- src/middlewares/Auth/index.js
+++ src/middlewares/Auth/index.js
@@ -3,25 +3,33 @@ const config = require('./config')
 const { getUrl } = require('@/utils/CommomUtils')
 const { tokenRedis } = require('@/db/redis')
 
+const router = require('@/routers/index')
+const routers = router.stack.map((item) => item.path)
+
 module.exports = async function (ctx, next) {
   let { url } = ctx
-  console.log(ctx.req)
-  if (config.whiteList.indexOf(getUrl(url)) === -1) {
-    try {
-      let token = ctx.get('Authorization')
-      let user = await tokenRedis.get(token)
-      if (!user) {
-        ctx.fail({ message: '登录已过期，请重新登录' })
-      } else if (user.expires < Date.now()) {
-        tokenRedis.destroy(token)
-        ctx.fail({ message: '登录已过期，请重新登录' })
-      } else {
-        await next()
+
+  // 判断当前路径是否为已注册路由
+  if (routers.indexOf(getUrl(url)) !== -1) {
+    if (config.whiteList.indexOf(getUrl(url)) === -1) {
+      try {
+        let token = ctx.get('Authorization')
+        let user = await tokenRedis.get(token)
+        if (!user) {
+          ctx.fail({ message: '登录已过期，请重新登录' })
+        } else if (user.expires < Date.now()) {
+          tokenRedis.destroy(token)
+          ctx.fail({ message: '登录已过期，请重新登录' })
+        } else {
+          await next()
+        }
+      } catch (error) {
+        ctx.fail({ message: 'token is error, please relogin', status: 401 })
       }
-    } catch (error) {
-      ctx.fail({ message: 'token is error, please relogin', status: 401 })
+    } else {
+      await next()
     }
   } else {
-    await next()
+    ctx.fail({ message: '404 not found', code: 404, status: 404 })
   }
 }
