3feee434a745af07df39480a8e85733a9f519aef
luoxikui <luoxikui@emay.cn>
2021-11-02 09:43:26 +0800

    权限管理添加权限限制

--- src/routers/router/system/authRouter.js
+++ src/routers/router/system/authRouter.js
@@ -1,13 +1,14 @@
 const Router = require('@koa/router')
 const authController = require('@/controllers/system/authController')
 const LoginVerify = require('@/middlewares/LoginVerify')
+const AuthVerify = require('@/middlewares/AuthVerify')
 
 const authRouter = new Router({ prefix: '/system' })
 
 authRouter
-  .post('/auth/tree', LoginVerify, authController.tree)
-  .post('/auth/add', LoginVerify, authController.add)
-  .post('/auth/update', LoginVerify, authController.update)
-  .post("/auth/delete", LoginVerify, authController.delete)
+  .post('/auth/tree', LoginVerify, AuthVerify('SYSTEM_AUTH_LIST'), authController.tree)
+  .post('/auth/add', LoginVerify, AuthVerify('SYSTEM_AUTH_ADD'), authController.add)
+  .post('/auth/update', LoginVerify, AuthVerify('SYSTEM_AUTH_UPDATE'), authController.update)
+  .post('/auth/delete', LoginVerify, AuthVerify('SYSTEM_AUTH_DELETE'), authController.delete)
 
 module.exports = authRouter
