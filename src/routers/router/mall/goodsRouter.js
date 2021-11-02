5ee60a1b0621533364881e27b568009e63380308
luoxikui <luoxikui@emay.cn>
2021-11-02 17:16:01 +0800

    商城模块-商品

--- src/routers/router/mall/goodsRouter.js
+++ src/routers/router/mall/goodsRouter.js
@@ -1,9 +1,13 @@
 const Router = require('@koa/router')
-const goodsController = require("@/controllers/mall/goodsController")
+const goodsController = require('@/controllers/mall/goodsController')
 
 const goodsRouter = new Router({ prefix: '/mall/goods' })
 
-goodsRouter.post("/add", goodsController.add).post("/list", goodsController.list)
-
+goodsRouter
+  .post('/add', goodsController.add)
+  .post('/list', goodsController.list)
+  .post('/detail', goodsController.detail)
+  .post('/update', goodsController.update)
+  .post('/delete', goodsController.delete)
 
 module.exports = goodsRouter
